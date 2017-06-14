88914e3f77b6e4272317f6342c5834496000a899
Michelle Golle <michelleg54@gmail.com>
2017-06-14 15:17:14 -0600

    gives admin user ability to creat users with multiple roles

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,21 @@ module Admin
     skip_before_action :require_admin!, only: [:stop_impersonating]
     respond_to :html, :json
 
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "User created successfully"
+        redirect_to admin_users_path
+      else
+        flash[:error] = "Invalid fields"
+        render :new
+      end
+    end
+
     def index
       @users = User.all
 
@@ -32,6 +47,15 @@ module Admin
         impersonated_user_email: user.email,
         impersonated_by_email: true_user.email,
       )
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name,
+                            :last_name,
+                            :email,
+                            :password,
+                            :password_confirmation,
+                            roles: [])
     end
   end
 end
